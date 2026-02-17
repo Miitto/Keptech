76b1df120a5a5a8266280f2f915f1ec65619c03d
Miitto <wpbickerton@gmail.com>
2026-02-17 00:50:11 +0000

    fix: Incorrect gBuffer normals

--- core/include/keptech/core/components/lights.hpp
+++ core/include/keptech/core/components/lights.hpp
@@ -4,8 +4,8 @@
 
 namespace keptech::components {
   struct PointLight {
-    glm::vec3 color;
-    float intensity;
-    float radius;
+    glm::vec3 color{1.f, 1.f, 1.f};
+    float intensity = 1.f;
+    float radius = 1.f;
   };
 } // namespace keptech::components
