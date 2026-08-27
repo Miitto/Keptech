482e7c97b57876103c0d85ae12143d8fde983d6c
Miitto <wpbickerton@gmail.com>
2026-08-27 17:33:13 +0100

    fix: cmake SDL found

--- cmake/sdl.cmake
+++ cmake/sdl.cmake
@@ -1,6 +1,6 @@
 find_package(SDL3 QUIET)
 
-if (NOT SDL3)
+if (NOT SDL3_FOUND)
   message(STATUS "Downloading SDL3")
   # SDL logs a lot of stuff to the console, so we temporarily set the log level to WARNING to avoid cluttering the output
   cmake_language(GET_MESSAGE_LOG_LEVEL PRE_SDL_FETCH_LOG_LEVEL)
@@ -15,7 +15,7 @@ if (NOT SDL3)
   )
   FetchContent_MakeAvailable(sdl)
   set(CMAKE_MESSAGE_LOG_LEVEL ${PRE_SDL_FETCH_LOG_LEVEL})
-  set(SDL3 TRUE)
+  set(SDL3_FOUND TRUE)
 endif()
 
 function(link_sdl TARGET_NAME ACCESS)
